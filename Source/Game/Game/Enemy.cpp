eddd497735da8f38741614d9292ebc8b0e3bbad2
Marcus Reyes <mreyes@student.neumont.edu>
2023-08-01 20:41:46 -0600

    Add project files.

---
+++ Source/Game/Game/Enemy.cpp
@@ -0,0 +1,64 @@
+#include "Enemy.h"
+#include "Player.h"
+#include "SpaceGame.h"
+#include "Framework/Scene.h"
+#include "Framework/Emitter.h"
+#include "Renderer/Renderer.h"
+
+
+void Enemy::Update(float dt)
+{
+	Actor::Update(dt);
+
+	kiko::vec2 forward = kiko::vec2{ 0, -1 }.Rotate(m_transform.rotation);
+	Player* player = m_scene->GetActor<Player>();
+	if (player)
+	{
+		kiko::vec2 direction = player->m_transform.position - m_transform.position;
+		// turn towards player		
+		float turnAngle = kiko::vec2::SignedAngle(forward, direction.Normalized());
+		m_transform.rotation += turnAngle * dt;
+		// check if player is in front
+		if (std::fabs(turnAngle) < kiko::DegreesToRadians(30.0f))
+		{
+			// I see you!
+		}
+
+	}
+
+	m_transform.position += forward * m_speed * kiko::g_time.GetDeltaTime();
+	m_transform.position.x = kiko::Wrap(m_transform.position.x, (float)kiko::g_renderer.GetWidth());
+	m_transform.position.y = kiko::Wrap(m_transform.position.y, (float)kiko::g_renderer.GetHeight());
+
+
+}
+
+void Enemy::OnCollision(Actor* other)
+{
+	if (other->m_tag == "Player")
+	{
+		m_game->AddPoints(100);
+		m_destroyed = true;
+
+		// create explosion
+		kiko::EmitterData data;
+		data.burst = true;
+		data.burstCount = 100;
+		data.spawnRate = 0;
+		data.angle = 0;
+		data.angleRange = kiko::Pi;
+		data.lifetimeMin = 0.5f;
+		data.lifetimeMin = 1.5f;
+		data.speedMin = 50;
+		data.speedMax = 250;
+		data.damping = 0.5f;
+
+		data.color = kiko::Color{ 1, 1, 1, 1 };
+
+		kiko::Transform transform{ m_transform.position, 0, 1};
+		auto emitter = std::make_unique<kiko::Emitter>(transform, data);
+		emitter->m_lifespan = 0.1f;
+		m_scene->Add(std::move(emitter));
+
+	}
+}
---
+++ Source/Game/Game/Main.cpp
@@ -0,0 +1,134 @@
+#include "Core/Core.h"
+#include "Renderer/Renderer.h"
+#include "Renderer/ModelManager.h"
+#include "Input/InputSystem.h"
+#include "Audio/AudioSystem.h"
+#include "Framework/Scene.h"
+#include "Framework/Emitter.h"
+#include "Renderer/Text.h"
+#include "Renderer/ParticleSystem.h"
+#include "Player.h"
+#include "Enemy.h"
+#include "SpaceGame.h"
+
+#include <iostream>
+#include <vector>
+#include <thread>
+
+using namespace std;
+
+class Star
+{
+public:
+	Star(const kiko::vec2& pos, const kiko::vec2& vel) :
+		m_pos{ pos },
+		m_vel{ vel }
+	{}
+
+	void Update(int width, int height)
+	{
+		m_pos += m_vel * kiko::g_time.GetDeltaTime();
+		if (m_pos.x >= width) m_pos.x = 0;
+		if (m_pos.y >= height) m_pos.y = 0;
+	}
+
+	void Draw(kiko::Renderer& renderer)
+	{
+		renderer.DrawPoint(m_pos.x, m_pos.y);
+	}
+
+public:
+	kiko::vec2 m_pos;
+	kiko::vec2 m_vel;
+};
+
+
+
+int main(int argc, char* argv[])
+{
+	INFO_LOG;
+
+	kiko::MemoryTracker::Initialize();
+	kiko::seedRandom((unsigned int)time(nullptr));
+	kiko::setFilePath("assets");
+
+	// initialize engine
+	kiko::g_renderer.Initialize();
+	kiko::g_renderer.CreateWindow("CSC196", 800, 600);
+
+	kiko::g_inputSystem.Initialize();
+	kiko::g_audioSystem.Initialize();
+
+	// create game
+	unique_ptr<SpaceGame> game = make_unique<SpaceGame>();
+	game->Initialize();
+
+	vector<Star> stars;
+	for (int i = 0; i < 1000; i++)
+	{
+		kiko::Vector2 pos(kiko::Vector2(kiko::random(kiko::g_renderer.GetWidth()), kiko::random(kiko::g_renderer.GetHeight())));
+		kiko::Vector2 vel(kiko::randomf(100, 200), 0.0f);
+
+		stars.push_back(Star(pos, vel));
+	}
+
+	// main game loop
+	bool quit = false;
+	while (!quit)
+	{
+		// update engine
+		kiko::g_time.Tick();
+		kiko::g_inputSystem.Update();
+		if (kiko::g_inputSystem.GetKeyDown(SDL_SCANCODE_ESCAPE))
+		{
+			quit = true;
+		}
+		kiko::g_particleSystem.Update(kiko::g_time.GetDeltaTime());
+
+		// update game
+		game->Update(kiko::g_time.GetDeltaTime());
+
+		// draw game
+		kiko::g_renderer.SetColor(0, 0, 0, 0);
+		kiko::g_renderer.BeginFrame();
+		for (auto& star : stars)
+		{
+			star.Update(kiko::g_renderer.GetWidth(), kiko::g_renderer.GetHeight());
+			kiko::g_renderer.SetColor(kiko::random(256), kiko::random(256), kiko::random(256), 255);
+			star.Draw(kiko::g_renderer);
+		}
+
+		game->Draw(kiko::g_renderer);
+		kiko::g_particleSystem.Draw(kiko::g_renderer);
+		
+		kiko::g_renderer.EndFrame();
+	}
+
+	stars.clear();
+
+	return 0;
+}
+/*
+		if (kiko::g_inputSystem.GetMouseButtonDown(0) &&
+			!kiko::g_inputSystem.GetPreviousMouseButtonDown(0))
+		{
+			kiko::EmitterData data;
+			data.burst = true;
+			data.burstCount = 100;
+			data.spawnRate = 200;
+			data.angle = 0;
+			data.angleRange = kiko::Pi;
+			data.lifetimeMin = 0.5f;
+			data.lifetimeMin = 1.5f;
+			data.speedMin = 50;
+			data.speedMax = 250;
+			data.damping = 0.5f;
+
+			data.color = kiko::Color{ 1, 1, 1, 1 };
+
+			kiko::Transform transform{ { kiko::g_inputSystem.GetMousePosition() }, 0, 1};
+			auto emitter = std::make_unique<kiko::Emitter>(transform, data);
+			emitter->m_lifespan = 1.0f;
+			scene.Add(std::move(emitter));
+		}
+*/
